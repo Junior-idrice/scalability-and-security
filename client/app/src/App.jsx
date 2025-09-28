e06c1f373a984e910e2d09f70884cd02309a0919
idrice junior <dongmojunior18@gmail.com>
2025-09-27 19:28:51 -0500

    t

---
+++ client/app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1> Books website</h1>
+    <div>
+      <input type="text" placeholder="book title..."/>
+      <input type="number" placeholder="release date"/>
+      <button>add book</button>
+    </div>
+     </>
+  )
+}
+
+export default App
