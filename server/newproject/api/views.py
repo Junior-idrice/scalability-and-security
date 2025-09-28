4371c665bca74dd4a15e87c0d0f3de4479d74fa9
idrice junior <dongmojunior18@gmail.com>
2025-09-27 19:59:09 -0500

    working

--- server/newproject/api/views.py
+++ server/newproject/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+# ✅ correct imports
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializer import BookSerializer
+
+
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True).data
+    return Response(serializer)
